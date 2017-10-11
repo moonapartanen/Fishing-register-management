07b4c8f722d4e2f5cf165a8aca9c98c4b4cd2958
Hepeja <henri.jarvelainen@edu.savonia.fi>
2017-10-11 13:36:54 +0300

    Formi muutettu normiksi ja hallintapaneelia aloitettu

--- Kalavale/Form1.Designer.cs
+++ Kalavale/Form1.Designer.cs
@@ -28,122 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.metroButton1 = new MetroFramework.Controls.MetroButton();
-            this.metroButton2 = new MetroFramework.Controls.MetroButton();
-            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
-            this.metroButton3 = new MetroFramework.Controls.MetroButton();
-            this.metroTabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbDBM = new System.Windows.Forms.ComboBox();
+            this.dgDBM = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDBM)).BeginInit();
             this.SuspendLayout();
             // 
-            // metroTabControl1
+            // tabControl1
             // 
-            this.metroTabControl1.Controls.Add(this.tabPage1);
-            this.metroTabControl1.Controls.Add(this.tabPage2);
-            this.metroTabControl1.Controls.Add(this.tabPage3);
-            this.metroTabControl1.Location = new System.Drawing.Point(23, 63);
-            this.metroTabControl1.Name = "metroTabControl1";
-            this.metroTabControl1.SelectedIndex = 0;
-            this.metroTabControl1.Size = new System.Drawing.Size(936, 526);
-            this.metroTabControl1.TabIndex = 0;
-            this.metroTabControl1.UseSelectable = true;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(980, 596);
+            this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.metroButton3);
-            this.tabPage1.Controls.Add(this.metroComboBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 38);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Size = new System.Drawing.Size(928, 484);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(972, 570);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Datan analysointi";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.metroButton2);
-            this.tabPage2.Controls.Add(this.metroButton1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 38);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Size = new System.Drawing.Size(928, 484);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(972, 570);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Lomakkeiden hallinta";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 38);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Controls.Add(this.dgDBM);
+            this.tabPage3.Controls.Add(this.panel1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(928, 484);
+            this.tabPage3.Size = new System.Drawing.Size(972, 570);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Tietokannan hallinta";
+            this.tabPage3.Text = "Tietokannan Hallinta";
+            this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // metroButton1
+            // panel1
             // 
-            this.metroButton1.Location = new System.Drawing.Point(46, 40);
-            this.metroButton1.Name = "metroButton1";
-            this.metroButton1.Size = new System.Drawing.Size(162, 57);
-            this.metroButton1.TabIndex = 0;
-            this.metroButton1.Text = "UUSI LOMAKE";
-            this.metroButton1.UseSelectable = true;
+            this.panel1.Controls.Add(this.cmbDBM);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 328);
+            this.panel1.TabIndex = 0;
             // 
-            // metroButton2
+            // label1
             // 
-            this.metroButton2.Location = new System.Drawing.Point(48, 109);
-            this.metroButton2.Name = "metroButton2";
-            this.metroButton2.Size = new System.Drawing.Size(160, 61);
-            this.metroButton2.TabIndex = 1;
-            this.metroButton2.Text = "LOMAKKEIDEN SELAILU";
-            this.metroButton2.UseSelectable = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valitse muokattava";
             // 
-            // metroComboBox1
+            // cmbDBM
             // 
-            this.metroComboBox1.FormattingEnabled = true;
-            this.metroComboBox1.ItemHeight = 23;
-            this.metroComboBox1.Location = new System.Drawing.Point(57, 44);
-            this.metroComboBox1.Name = "metroComboBox1";
-            this.metroComboBox1.Size = new System.Drawing.Size(209, 29);
-            this.metroComboBox1.TabIndex = 0;
-            this.metroComboBox1.UseSelectable = true;
+            this.cmbDBM.FormattingEnabled = true;
+            this.cmbDBM.Location = new System.Drawing.Point(35, 44);
+            this.cmbDBM.Name = "cmbDBM";
+            this.cmbDBM.Size = new System.Drawing.Size(121, 21);
+            this.cmbDBM.TabIndex = 1;
+            this.cmbDBM.SelectedIndexChanged += new System.EventHandler(this.cmbDBM_SelectedIndexChanged);
             // 
-            // metroButton3
+            // dgDBM
             // 
-            this.metroButton3.Location = new System.Drawing.Point(57, 91);
-            this.metroButton3.Name = "metroButton3";
-            this.metroButton3.Size = new System.Drawing.Size(157, 75);
-            this.metroButton3.TabIndex = 2;
-            this.metroButton3.Text = "HAE";
-            this.metroButton3.UseSelectable = true;
+            this.dgDBM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDBM.Location = new System.Drawing.Point(340, 4);
+            this.dgDBM.Name = "dgDBM";
+            this.dgDBM.Size = new System.Drawing.Size(629, 328);
+            this.dgDBM.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Poista";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(421, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Muokkaa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(340, 338);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Lisää";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(982, 599);
-            this.Controls.Add(this.metroTabControl1);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "KalaVale 1.0";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.metroTabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDBM)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
-        private MetroFramework.Controls.MetroButton metroButton3;
-        private MetroFramework.Controls.MetroComboBox metroComboBox1;
-        private MetroFramework.Controls.MetroButton metroButton2;
-        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgDBM;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbDBM;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Kalavale/Form1.cs
+++ Kalavale/Form1.cs
@@ -11,8 +11,29 @@ using MetroFramework.Forms;
 
 namespace Kalavale
 {
-    public partial class Form1 : MetroForm
+    public partial class Form1 : Form
     {
+        private string[] dbmItems = { "Kalat", "Pyydykset", "Käyttäjät" };
+        DBConnection db = new DBConnection();
+
+        public void UpdateDataGridViews()
+        {
+
+            int index = cmbDBM.SelectedIndex;
+            switch(index)
+            {
+                case 0:
+                    dgDBM.DataSource = db.Select(db.dbmFishQuery);
+                    break;
+                case 1:
+                    dgDBM.DataSource = db.Select(db.dbmMethodQuery);
+                    break;
+                case 2:
+                    dgDBM.DataSource = db.Select(db.dbmUserQuery);
+                    break;            
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +41,15 @@ namespace Kalavale
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            db.OpenConnection();
+            cmbDBM.DataSource = dbmItems;
+            UpdateDataGridViews();
+            
+        }
 
+        private void cmbDBM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateDataGridViews();
         }
     }
 }
