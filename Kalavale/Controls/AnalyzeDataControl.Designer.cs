21e0efa4397f8f6b9737d37c82fe89f4d0863ce0
Moona E Partanen <Moona.Partanen@edu.savonia.fi>
2017-11-29 14:50:21 +0200

    Raportointia aloitettu, Excelpohjakoodit valmiina

--- Kalavale/Controls/AnalyzeDataControl.Designer.cs
+++ Kalavale/Controls/AnalyzeDataControl.Designer.cs
@@ -28,31 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.cboResearchArea = new System.Windows.Forms.ComboBox();
+            this.txtGroupSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboSurvey = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
             // 
-            // button1
+            // btnAnalyze
             // 
-            this.button1.Location = new System.Drawing.Point(251, 154);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Location = new System.Drawing.Point(21, 389);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 0;
+            this.btnAnalyze.Text = "Analysoi";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // cboResearchArea
+            // 
+            this.cboResearchArea.DisplayMember = "Name";
+            this.cboResearchArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboResearchArea.FormattingEnabled = true;
+            this.cboResearchArea.Location = new System.Drawing.Point(21, 55);
+            this.cboResearchArea.Name = "cboResearchArea";
+            this.cboResearchArea.Size = new System.Drawing.Size(170, 21);
+            this.cboResearchArea.TabIndex = 1;
+            this.cboResearchArea.ValueMember = "Id";
+            this.cboResearchArea.SelectedIndexChanged += new System.EventHandler(this.cboResearchArea_SelectedIndexChanged);
+            // 
+            // txtGroupSize
+            // 
+            this.txtGroupSize.Location = new System.Drawing.Point(21, 168);
+            this.txtGroupSize.Name = "txtGroupSize";
+            this.txtGroupSize.Size = new System.Drawing.Size(170, 20);
+            this.txtGroupSize.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valitse tutkimusalue:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Anna perusjoukon koko:";
+            // 
+            // cboSurvey
+            // 
+            this.cboSurvey.DisplayMember = "Name";
+            this.cboSurvey.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSurvey.FormattingEnabled = true;
+            this.cboSurvey.Location = new System.Drawing.Point(20, 111);
+            this.cboSurvey.Name = "cboSurvey";
+            this.cboSurvey.Size = new System.Drawing.Size(171, 21);
+            this.cboSurvey.TabIndex = 5;
+            this.cboSurvey.ValueMember = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Valitse kysely:";
             // 
             // AnalyzeDataControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboSurvey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGroupSize);
+            this.Controls.Add(this.cboResearchArea);
+            this.Controls.Add(this.btnAnalyze);
             this.Name = "AnalyzeDataControl";
-            this.Size = new System.Drawing.Size(502, 293);
+            this.Size = new System.Drawing.Size(683, 440);
+            this.Load += new System.EventHandler(this.AnalyzeDataControl_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.ComboBox cboResearchArea;
+        private System.Windows.Forms.TextBox txtGroupSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboSurvey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
--- Kalavale/Controls/AnalyzeDataControl.cs
+++ Kalavale/Controls/AnalyzeDataControl.cs
@@ -3,14 +3,84 @@ using System.Windows.Forms;
 using Kalavale.Forms;
 using Kalavale.Repositories;
 using Kalavale.Entities;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Kalavale.Controls
-{
+{  
     public partial class AnalyzeDataControl : UserControl
     {
+        ResearchAreaRepository _raRepository = new ResearchAreaRepository();
+        SurveyRepository _sRepository = new SurveyRepository();
+
+        Excel.Application excel;
+
         public AnalyzeDataControl()
         {
             InitializeComponent();
         }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            ResearchArea ra = cboResearchArea.SelectedItem as ResearchArea;
+            Survey s = cboSurvey.SelectedItem as Survey;
+            double groupSize = Double.Parse(txtGroupSize.Text);
+            ToExcel(ra, s, groupSize);
+        }
+
+        private void AnalyzeDataControl_Load(object sender, EventArgs e)
+        {
+            cboResearchArea.DataSource = _raRepository.GetAll();
+        }
+
+        private void cboResearchArea_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ResearchArea ra = cboResearchArea.SelectedItem as ResearchArea;
+            cboSurvey.DataSource = _sRepository.GetByResearchArea(ra);
+        }
+
+        private void ToExcel(ResearchArea ra, Survey s, double groupSize)
+        {
+
+            excel = new Excel.Application();
+
+            if (excel == null)
+            {
+                MessageBox.Show("Excel-ohjelmaa ei ole asennettu!");
+            }
+            else
+            {
+                Excel.Workbook xlWorkBook;
+                Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+
+                xlWorkBook = excel.Workbooks.Add(misValue);
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                xlWorkSheet.Cells[1, 1] = "ID";
+                xlWorkSheet.Cells[1, 2] = "Name";
+                xlWorkSheet.Cells[2, 1] = "1";
+                xlWorkSheet.Cells[2, 2] = "One";
+                xlWorkSheet.Cells[3, 1] = "2";
+                xlWorkSheet.Cells[3, 2] = "Two";
+
+
+
+                xlWorkBook.SaveAs(saveFileDialog1.ShowDialog());
+                // TODO: TÄHÄN TIEDOSTONIMEN JA PATHIN TALLENNUS
+                xlWorkBook.Close(true);
+
+                Marshal.ReleaseComObject(xlWorkSheet);
+                Marshal.ReleaseComObject(xlWorkBook);
+                Marshal.ReleaseComObject(excel);
+
+                MessageBox.Show("Excel file created");
+
+                // TO DO: EXCEL PITÄÄ SAADA SULKEUTUMAAN JOLLAIN KONSTILLA
+            }        
+        }
+
+
+
     }
 }
